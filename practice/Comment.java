555edcbf69252f44a6e548a725f3db96c29cdc93
KTH <taehyeonentus@gmail.com>
2024-01-12 15:54:40 +0900

    Done

---
+++ practice/Comment.java
@@ -0,0 +1,8 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("hello java1");  //주석을 달아봅시다.
+//        System.out.println("hello java2");
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+    }
+}
